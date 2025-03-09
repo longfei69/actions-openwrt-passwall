86c5155479dde1badc65e02d8fe504b0d8403a10
longfei69 <longfei_2@126.com>
2025-03-09 23:31:02 +0800

    diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
